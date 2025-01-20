98fdf223ec6a0d7bb96b73c7e6bf3d5d7082f63c
Rodrigo-Caldas <rodrigocaldas@id.uff.br>
2025-01-20 04:08:58 -0300

    :hammer: Caminho_contorno é uma variável

--- ana/__main__.py
+++ ana/__main__.py
@@ -10,7 +10,13 @@ from ana.config import config
 from ana.logit import console
 
 
-async def handler(data_inicio: str = "01-01-2024", data_fim: str = "") -> None:
+async def handler(
+    data_inicio: str = "01-01-2024",
+    data_fim: str = "",
+    caminho_contorno: Path = Path(
+        "mapas", "sub-bacias-isoladas", "grande", "agua_vermelha.shp"
+    ),
+) -> None:
     """
     Execução do serviço de coleta da chuva horária dos postos em operação da ANA.
 
@@ -20,19 +26,19 @@ async def handler(data_inicio: str = "01-01-2024", data_fim: str = "") -> None:
         Data de início da requisição dos dados, by default '01-01-2024'.
     data_fim : str, optional
         Data final da requisição dos dados, by default "".
+    caminho_contorno : Path, optional
+        Caminho do shapefile ou do diretório contendo shapefiles, by default Path("mapas", "sub-bacias-isoladas", "grande", "agua_vermelha.shp").
     """
     console.rule("Iniciando serviço de coleta de chuva horária da ANA.")
 
     df_inventario = ana.mostrar_inventario()
 
-    caminho_base = Path("mapas", "sub-bacias-isoladas", "grande", "agua_vermelha.shp")
-
-    if caminho_base.is_file():
+    if caminho_contorno.is_file():
         caminhos = []
-        caminhos.append(caminho_base)
+        caminhos.append(caminho_contorno)
 
-    elif caminho_base.is_dir():
-        caminhos = list(caminho_base.rglob("*.shp"))
+    elif caminho_contorno.is_dir():
+        caminhos = list(caminho_contorno.rglob("*.shp"))
 
     for shp in caminhos:
         console.rule(f"Obtendo dados de {shp.name}")
